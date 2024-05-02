485965ef58cff732979678c279b1a4b104b7d735
jmodesto <jordano.modesto@imio.be>
2024-05-02 11:14:28 +0200

    [TELE-1877]Add input in install script to avoid manual input in queries

--- teleservices_iacitizen/install_teleservices_iacitizen.sh
+++ teleservices_iacitizen/install_teleservices_iacitizen.sh
@@ -2,6 +2,19 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
+# Ask for the commune slug
+echo "Veuillez entrer le slug de la commune :"
+read commune_slug
+
+# List of selected files
+selected_files=("restapi_actualites.json" "restapi_annuaire.json" "restapi_evenements.json.json")
+
+# Use a loop to apply the change to all selected files
+for file in "${selected_files[@]}"; do
+    # Use sed to replace "uri": "saintvith" with "uri": "value_of_COMMUNE_SLUG" in the file
+    sed -i "s/\"uri\": \"saintvith\"/\"uri\": \"$commune_slug\"/g" "$file"
+done
+
 # installation path
 install_path="/usr/lib/teleservices_iacitizen"
 
